e09fe457fe5ef8278a7a612a9d6ac3c4fac7ca90
nene <52809438@qq.com>
2022-07-27 13:43:53 +0800

    Initial commit

---
+++ src/main/java/com/example/demothymeleaf/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.example.demothymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+	@GetMapping("/index")
+	public String getIndex(Model model) {
+
+		model.addAttribute("name", "ne");
+		return "index";
+	}
+}
